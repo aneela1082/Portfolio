9704a6e76b0263833f8483ab8d4fbfd31a55db60
aneela1082 <85185037+aneela1082@users.noreply.github.com>
2023-10-25 01:19:50 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,29 @@
+ //NavScroll............
+ let nav= document.querySelector('nav');
+
+ window.addEventListener('scroll',()=>{
+     nav.classList.toggle('nav-scroll',window.scrollY>0);
+ });
+
+ //Active Toggle
+ let AllNavLinkA = document.querySelectorAll('.nav-links li a');
+
+ //Active Add
+ AllNavLinkA.forEach(item=>{
+     item.addEventListener('click',()=>{
+         removeActive();
+         item.classList.add('active');
+     })
+ });
+
+ //Active remove
+ const removeActive =()=>AllNavLinkA.forEach(item=>{
+     item.classList.remove('active');
+ });
+
+ //Nav Links Toggle........
+ let menuBars=document.querySelector('nav button');
+ let navlinks = document.querySelector('.nav-links');
+ menuBars.addEventListener('click',()=>{
+     navlinks.classList.toggle('nav-link-block');
+ });
